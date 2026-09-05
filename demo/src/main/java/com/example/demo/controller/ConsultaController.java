e2887d7f7e52a93f7f3453641ca305ea12efbcbb
jonasguilhermino509 <jonasguilhermino509@gmail.com>
2026-09-05 12:44:42 -0300

    ConsultaController.java

--- demo/src/main/java/com/example/demo/controller/ConsultaController.java
+++ demo/src/main/java/com/example/demo/controller/ConsultaController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/consultas")
 public class ConsultaController {
 
-    // O Autowired liga o nosso controlador com os repositórios que você criou antes
+
     @Autowired
     private ProfessorRepository professorRepository;
 
@@ -24,17 +24,15 @@ public class ConsultaController {
     @Autowired
     private HorarioRepository horarioRepository;
 
-    // --- VISUALIZAÇÃO POR PROFESSOR ---
-
-    // 1. Abre a tela para o usuário escolher um professor na lista
+   
     @GetMapping("/professor")
     public String buscarPorProfessor(Model model) {
-        // Pega todos os professores do banco e envia para a tela HTML
+        
         model.addAttribute("professores", professorRepository.findAll());
         return "consulta-professor"; 
     }
 
-    // 2. Mostra a tabela de horários daquele professor específico que foi clicado
+  
     @GetMapping("/professor/{id}")
     public String resultadoProfessor(@PathVariable Long id, Model model) {
         model.addAttribute("professor", professorRepository.findById(id).orElse(null));
@@ -42,16 +40,14 @@ public class ConsultaController {
         return "resultado-professor"; 
     }
 
-    // --- VISUALIZAÇÃO POR TURMA ---
+   
 
-    // 3. Abre a tela para o usuário escolher uma turma na lista
     @GetMapping("/turma")
     public String buscarPorTurma(Model model) {
         model.addAttribute("turmas", turmaRepository.findAll());
         return "consulta-turma";
     }
 
-    // 4. Mostra a tabela de horários daquela turma específica que foi clicada
     @GetMapping("/turma/{id}")
     public String resultadoTurma(@PathVariable Long id, Model model) {
         model.addAttribute("turma", turmaRepository.findById(id).orElse(null));
